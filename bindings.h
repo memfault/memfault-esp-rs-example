3de08177fdb82a6c9e9517bca625909da47f6b74
Blake Hildebrand <blake@memfault.com>
2024-08-29 16:12:09 -0400

    feat: add full metric example
    Add a full working example for setting metrics. These metrics are
    generated via FFI and can be set directly in rust code.

--- bindings.h
+++ bindings.h
@@ -1,4 +1,16 @@
 #pragma once
 
+// These files are required for the preprocessor to evaluate correctly. Normally
+// these are injected into the project via CMAKE, but that is bypassed here for
+// the bindings in esp-rs.
+#define MEMFAULT_METRICS_USER_HEARTBEAT_DEFS_FILE "memfault_esp_metrics_heartbeat_config.def"
+#define MEMFAULT_PLATFORM_CONFIG_FILE "memfault_esp_idf_port_config.h"
+#define MEMFAULT_TRACE_REASON_USER_DEFS_FILE "memfault_trace_reason_esp_idf_port_config.def"
+
+// Only required for ESP-IDF v4.4 and earlier.
+#define MEMFAULT_FREERTOS_COLLECT_TIMER_STACK_FREE_BYTES 0
+
 #include "external/memfault-firmware-sdk/ports/esp_idf/memfault/include/memfault/esp_port/core.h"
 #include "external/memfault-firmware-sdk/ports/esp_idf/memfault/include/memfault/esp_port/http_client.h"
+#include "external/memfault-firmware-sdk/components/include/memfault/panics/coredump.h"
+#include "external/memfault-firmware-sdk/components/include/memfault/metrics/metrics.h"
--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,12 @@ use esp_idf_hal::peripherals::Peripherals;
 use esp_idf_svc::wifi::{BlockingWifi, EspWifi};
 use esp_idf_svc::{eventloop::EspSystemEventLoop, nvs::EspDefaultNvsPartition};
 use esp_idf_sys as _; // If using the `binstart` feature of `esp-idf-sys`, always keep this module imported
-use esp_idf_sys::memfault::{memfault_boot, memfault_esp_port_http_client_post_data};
+use esp_idf_sys::memfault::{
+    memfault_boot, memfault_esp_port_http_client_post_data, memfault_metrics_heartbeat_debug_print,
+    memfault_metrics_heartbeat_debug_trigger, memfault_metrics_heartbeat_set_unsigned,
+    MemfaultMetricId, MfltMetricsIndex_kMfltMetricsIndex_test_metric,
+};
+use heapless::String;
 use log::*;
 
 const SSID: &str = env!("WIFI_SSID");
@@ -33,12 +38,19 @@ fn main() {
     connect_wifi(&mut wifi).unwrap();
 
     unsafe {
-        // Configures Memmfault. This is required to be called once before any other Memfault functionality
+        // Configures Memfault. This is required to be called once before any other Memfault functionality
         // is used.
         //
         // See here for implementation:
         // https://github.com/memfault/memfault-firmware-sdk/blob/bfc51680f260d4e47bfdf6fcf378239b25d99726/ports/esp_idf/memfault/common/memfault_platform_core.c#L157
         memfault_boot();
+
+        // Set a metric value. This will be sent to Memfault in the next heartbeat.
+        set_metric(MfltMetricsIndex_kMfltMetricsIndex_test_metric as i32);
+
+        memfault_metrics_heartbeat_debug_trigger();
+        memfault_metrics_heartbeat_debug_print();
+
         // Posts data to Memfault via the ESP-IDF HTTP Client. In this case we are just pushing crash
         // data. See below for a more in-depth look at how Memfault transports data.
         // https://docs.memfault.com/docs/mcu/data-from-firmware-to-the-cloud
@@ -55,12 +67,23 @@ fn main() {
     }
 }
 
+/// Sets a metric value to be sent to Memfault in the next heartbeat.
+///
+/// All metrics IDs are defined via C macros and will be exposed via FFI as
+/// MfltMetricsIndex_kMfltMetricsIndex_<metric_name>.
+fn set_metric(metric_id: i32) -> i32 {
+    unsafe {
+        let id = MemfaultMetricId { _impl: metric_id };
+        memfault_metrics_heartbeat_set_unsigned(id, 42)
+    }
+}
+
 fn connect_wifi(wifi: &mut BlockingWifi<EspWifi<'static>>) -> anyhow::Result<()> {
     let wifi_configuration: Configuration = Configuration::Client(ClientConfiguration {
-        ssid: SSID.into(),
+        ssid: String::try_from(SSID).unwrap(),
         bssid: None,
         auth_method: AuthMethod::WPA2Personal,
-        password: PASSWORD.into(),
+        password: String::try_from(PASSWORD).unwrap(),
         channel: None,
     });
 
